ebcad604d5297a75b9e896d8fd1f19b0734eb27b
songdo <songdo302@gmail.com>
2022-11-02 18:19:18 +0900

    8082 포트가 아니라 8081로만 실행되는거같은데 -> 아니었다
    ec2에서 80 포트를 안열었었네 그래서 그런가.....?

--- src/main/java/com/songdo/aws_study/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/songdo/aws_study/config/auth/dto/OAuthAttributes.java
@@ -66,7 +66,7 @@ public class OAuthAttributes {
     User 엔티티를 생성
     OAuthAttributes 에서 엔티티를 생성하는 시점은 처음 가입할 때
     가입할 때의 기본 권한을 GUEST로 주기 위해서 role 빌더값에는 Role.GUEST를 사용
-    OAuthAttributes 클래스 생성이 끝났으면 같은 퍀키지에 SessionUser 클래스를 생성
+    OAuthAttributes 클래스 생성이 끝났으면 같은 패키지에 SessionUser 클래스를 생성
      */
     public User toEntity() {
         return User.builder()
